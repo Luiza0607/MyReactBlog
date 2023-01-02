99ba68c4f30413ac1c3cedd4068c77f4a3e6cfe7
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-01-02 22:36:19 +0100

    initial project and add features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './components/pages/Home/Home.js';
+import { SinglePost } from './components/pages/SinglePost/SinglePost.js';
+import { PostAdd } from './components/pages/PostAdd/PostAdd.js';
+import { PostEdit } from './components/pages/PostEdit/PostEdit.js';
+import { About } from './components/pages/About/About.js';
+import { NotFound } from './components/pages/NotFound/NotFound.js';
+import { Container } from 'react-bootstrap';
+import { Header } from './components/views/Header/Header.js';
+import { Footer } from './components/views/Footer/Footer.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+	return (
+		<Container>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/post/:id' element={<SinglePost />} />
+				<Route path='/post/add' element={<PostAdd />} />
+				<Route path='/post/edit/:id' element={<PostEdit />} />
+				<Route path='/about' element={<About />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+			<Footer />
+		</Container>
+	);
 }
-
-export default App;
---
+++ src/redux/postsRedux.js
@@ -0,0 +1,13 @@
+//selectors
+export const getAllPosts = ({ posts }) => posts;
+
+// actions
+const createActionName = (actionName) => `app/posts/${actionName}`;
+
+// action creators
+export const postsReducer = (statePart = [], action) => {
+	switch (action.type) {
+		default:
+			return statePart;
+	}
+};
